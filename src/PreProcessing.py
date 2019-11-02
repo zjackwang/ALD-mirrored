afec4035378af81c3edfed8683a8f824b7b7fe3c
Reece Lehmann <reecelehmann@gmail.com>
2019-11-02 17:33:30 -0500

    sound files added

--- src/PreProcessing.py
+++ src/PreProcessing.py
@@ -3,11 +3,16 @@ files as they are input.
 For training data.
 """
 
-from __future__ import print_function
+#from __future__ import print_function
+import librosa.display
 import librosa
+import np
 
 # 1. Get the file path to the included audio example
-filename = "C:\Users\Reece\Documents\GitHub\ALD\Sound files\Slide Whistle 2 - Sound Effect.wav"
+data_folder = "C:\\Users\\Reece\\Documents\\GitHub\\ALD\\ALD\\src\\"
+filename = data_folder + "50FrenchClean.mp3"
+
+filename = librosa.util.example_audio_file()
 
 # 2. Load the audio as a waveform `y`
 #    Store the sampling rate as `sr`
@@ -19,4 +24,5 @@ tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
 print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
 
 # 4. Convert the frame indices of beat events into timestamps
-beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+D = np.abs(librosa.stft(y))
+
