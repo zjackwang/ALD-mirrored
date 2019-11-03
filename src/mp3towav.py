b43bd3cfbb2f39bde08ad73b70da61aa246ca8f3
Reece Lehmann <reecelehmann@gmail.com>
2019-11-02 19:32:57 -0500

    mp3towav sstart

--- src/mp3towav.py
+++ src/mp3towav.py
@@ -2,14 +2,27 @@
 
 
 def mp3towav(srcMP3File, destWavFile):
-    #TODO
-    pass
+    from os import path
+    from pydub import AudioSegment
+
+    # files
+    src = "transcript.mp3"
+    dst = "test.wav"
+
+    # convert wav to mp3
+    sound = AudioSegment.from_mp3(src)
+    sound.export(dst, format="wav")
+    
+    #https://pythonbasics.org/convert-mp3-to-wav/
 
 def saveSound(sound, wavFile):
     #TODO
     pass
 
 def wavFileToSound(wavFile):
+    #TODO
+    pass
+def playWavFile(wavFile):
     #TODO
     pass
 
